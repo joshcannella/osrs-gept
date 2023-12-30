22d3225b0e0c5c8a832fce3eb5b413c9aac11805
Josh Cannella <jcannella123@gmail.com>
2023-12-30 13:05:45 -0500

    test code

--- src/gept/catalog.py
+++ src/gept/catalog.py
@@ -4,3 +4,7 @@ class Catalog:
     def __init__(self):
         self.__api = PricesApi()
         self.mappings = self.__api.get_mappings()
+        
+if __name__ == '__main__':
+    c = Catalog()
+    print(c.mappings)
