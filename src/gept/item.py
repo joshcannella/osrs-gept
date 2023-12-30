fab1ec0914236b3813db731984139a13e6b89123
Josh Cannella <jcannella123@gmail.com>
2023-12-30 14:55:30 -0500

    init item

--- src/gept/item.py
+++ src/gept/item.py
@@ -1,2 +1,38 @@
+from prices_api import get_price_by_id
+
 class Item:
-    pass
+    def __init__(self, json):
+        self.info = json
+        self.name = json["name"]
+        self.id = json["id"]
+        
+    def __repr__(self):
+        return f"{self.name} ({self.id})"
+    
+    def get_price(self):
+        return get_price_by_id(self.id)
+    
+if __name__ == '__main__':
+    item = Item({
+        "examine": "It's a bar of gold.",
+        "id": 2357,
+        "members": False,
+        "lowalch": 120,
+        "limit": 10000,
+        "value": 300,
+        "highalch": 180,
+        "icon": "Gold bar.png",
+        "name": "Gold bar"
+    })
+    item2 = Item({
+        "examine": "A powerful staff.",
+        "id": 22647,
+        "members": True,
+        "lowalch": 120000,
+        "limit": 10,
+        "value": 300000,
+        "highalch": 180000,
+        "icon": "Zuriel's staff.png",
+        "name": "Zuriel's staff"
+    })
+    print(item.get_price())
