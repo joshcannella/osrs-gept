4196d5eae7e79ce5f3029022f470c22300e496a7
Josh Cannella <jcannella123@gmail.com>
2023-12-30 14:55:09 -0500

    parse items

--- src/gept/catalog.py
+++ src/gept/catalog.py
@@ -1,10 +1,14 @@
-from prices_api import PricesApi
+from item import Item
+from prices_api import get_mappings
 
 class Catalog:
     def __init__(self):
-        self.__api = PricesApi()
-        self.mappings = self.__api.get_mappings()
+        self.mappings = get_mappings()
+        self.items = self.get_items()
         
+    def get_items(self):
+        return {json['id']: Item(json) for json in self.mappings}
+    
 if __name__ == '__main__':
     c = Catalog()
-    print(c.mappings)
+    {print(i) for i in c.items.values()}
