7af1a6aca90cd076f2ec7e8fe20e814eb40c03bb
Josh Cannella <jcannella123@gmail.com>
2023-12-30 14:54:54 -0500

    refacor to module

--- src/gept/prices_api.py
+++ src/gept/prices_api.py
@@ -2,25 +2,27 @@ import json
 import urllib.request
 
 from constants import MAPPING_URL, PRICES_URL, VOLUMES_URL
+    
+def _fetch_json(url):
+    headers = {
+        "User-Agent" : "GE Price Tracker CLI - josh.cannella@yahoo.com"
+    }
 
-class PricesApi:
-    def __init__(self):
-        pass
-    
-    def __fetch_json(self, url):
-        headers = {
-            "User-Agent" : "GE Price Tracker CLI - josh.cannella@yahoo.com"
-        }
+    req = urllib.request.Request(url, headers=headers)
+    f = urllib.request.urlopen(req)
+    return json.load(f)
 
-        req = urllib.request.Request(url, headers=headers)
-        f = urllib.request.urlopen(req)
-        return json.load(f)
+def get_mappings():
+    return _fetch_json(MAPPING_URL)
     
-    def get_mappings(self):
-        return self.__fetch_json(MAPPING_URL)
-    
-    def get_prices(self):
-        return self.__fetch_json(PRICES_URL)['data']
-    
-    def get_volumes(self):
-        return self.__fetch_json(VOLUMES_URL)['data']
+def get_prices() -> dict:
+    return _fetch_json(PRICES_URL)['data']
+
+def get_volumes():
+    return _fetch_json(VOLUMES_URL)['data']
+
+def get_price_by_id(id):
+    try:
+        return get_prices()[str(id)]
+    except KeyError as e:
+        return None
